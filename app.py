b0dc65336fdbf9b42975376983a1f67e9dbac064
Christian-Encalada <cris_7715@hotmail.com>
2024-06-12 15:18:23 -0500

    deploy

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import os
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
